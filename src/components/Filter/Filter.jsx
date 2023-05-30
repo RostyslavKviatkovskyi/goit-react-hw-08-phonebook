d30f3a50c8cebb04dc7960b71888e80d31be0fe1
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2023-05-30 15:50:30 +0300

    fixed import

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,4 +1,5 @@
-import React, { useCallback } from 'react';
+// import React, { useCallback } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 // import { selectFilterName, setSearch } from 'redux/store';
