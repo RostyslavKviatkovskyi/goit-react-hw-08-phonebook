74877b492f501cfab852ebf5e14d1509d2f1b362
Rostyslav Kviatkovskyi <allencor1234@gmail.com>
2023-06-27 08:50:04 +0300

    browser router fix

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const theme = createTheme({
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <App />
       </BrowserRouter>
     </ThemeProvider>
